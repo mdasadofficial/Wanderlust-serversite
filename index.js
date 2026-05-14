cdabeb4b6e1f0720b0873a266e868cdc024044c1
mdasadofficial <mdasadofficial1@gmail.com>
2026-05-14 15:24:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,101 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+console.log(uri);
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("wanderlust");
+    const destinationCollection = db.collection("destination");
+
+    // Get Api
+    app.get("/destination", async (req, res) => {
+      const result = await destinationCollection.find().toArray();
+      res.json(result);
+    });
+
+    // Post Api
+    app.post("/destination", async (req, res) => {
+      const destinationData = req.body;
+      const result = await destinationCollection.insertOne(destinationData);
+      console.log(destinationData);
+      res.json(result);
+    });
+
+    //  Destination Id Api
+
+    app.get("/destination/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await destinationCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.json(result);
+    });
+
+
+    // Patch Api
+    app.patch("/destination/:id", async (req, res) => {
+      const { id } = req.params;
+      const updatedData = req.body;
+
+    console.log(updatedData);
+
+      const result = await destinationCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData },
+      );
+      res.json(result);
+     
+    });
+
+
+
+    // Delete Api
+app.delete('/destination/:id', async (req,res)=>{
+  const {id} = req.params;
+  const result = await destinationCollection.deleteOne({_id: new ObjectId(id)})
+  res.json(result);
+})
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
